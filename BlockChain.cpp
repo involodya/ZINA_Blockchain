4e15688e96e4bab65bb01c01ee0a5e0692dd2265
andzh1 <andrey.zhizhelev@yandex.ru>
2022-04-10 15:57:30 +0300

    Testes BlockChain

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -13,13 +13,9 @@ void BlockChain::_proceed_block_to_chain() {
     _verified_chain.push_block(_unverified_chain.get_block_to_push());
 }
 
-void BlockChain::add_block(const Block& block_to_add) { // #TODO How to push exactly in required branch of blockchain?
+void BlockChain::add_block(const Block& block_to_add) {
     _unverified_chain.add_block(block_to_add);
     if (_unverified_chain.length_of_max_chain() > MAX_LENGTH_OF_BRANCHING_CHAIN) {
         _proceed_block_to_chain();
     }
-}
-
-void printer() {
-    std::cout << "Blockchain works";
 }
